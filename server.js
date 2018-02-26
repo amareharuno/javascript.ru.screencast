8ffb92d55c15cf8d0d3b5e36d9a1a075d6038942
Lyuba Eremeeva <leremeeva174@gmail.com>
2018-02-27 00:14:54 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+let log = require('./logger')(module);
+let db = require('./db');
+db.connect();
+
+let User = require('./user');
+
+function run() {
+    let vasya = new User("Вася");
+    let petya = new User("Петя");
+
+    vasya.hello(petya);
+
+    log(db.getPhrase("Run successful"));
+}
+
+if (module.parent) {
+    exports.run = run;
+} else {
+    run();
+}
---
+++ user/index.js
@@ -0,0 +1,15 @@
+// module.exports = exports = this
+let db = require('../db');
+let log = require('../logger')(module);
+
+function User(name) {
+    this.name = name;
+}
+
+User.prototype.hello = function(who) {
+    // ...
+    log(db.getPhrase("Hello") + ", " + who.name);
+};
+
+// ...
+module.exports = User;
